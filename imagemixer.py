d8791092a4b2f44dc29f9ece04717dc0f257459d
aajarven <anni.jarvenpaa@gmail.com>
2018-05-05 21:56:53 +0300

    added help and options to mix either rows or columns

--- imagemixer.py
+++ imagemixer.py
@@ -3,12 +3,17 @@ from scipy import misc
 from optparse import OptionParser
 
 if __name__ == "__main__":
-	parser = OptionParser()
-	(options, args) = parser.parse_args()
+	parser = OptionParser(usage="python [options] imagemixer.py infile1.png infile2.png"
+					   + " outfile.png")
+	parser.add_option("-c", "--column", dest="swap_columns", default=False,
+				   action="store_true", help="Mix images column by column")
+	parser.add_option("-r", "--row", dest="swap_rows", default=False,
+				   action="store_true", help="Mix images row by row")
+	(opts, args) = parser.parse_args()
 
 	if len(args) < 3:
-		print("You must provide two input files and an output location, e.g." +
-		"\npython imagemixer.py inPic1.png inPic2.png output.png")
+		print("\nYou must provide two input files and an output location\n")
+		parser.print_help()
 		exit()
 	
 	infile1 = args[0]
@@ -19,14 +24,28 @@ if __name__ == "__main__":
 	img2 = misc.imread(infile2)
 
 	if img1.shape != img2.shape:
-		print("The images must have same dimensions. The dimensions of the" + 
+		print("\nThe images must have same dimensions. The dimensions of the" + 
 		" given images were:")
 		print(infile1 + ": " + str(img1.shape[1]) + " x " + str(img1.shape[0]))
 		print(infile2 + ": " + str(img2.shape[1]) + " x " + str(img2.shape[0]))
+		print()
+		parser.print_help()
 		exit()
 	else:
-		new_image = np.empty([img1.shape[0]*2, img1.shape[1], 3])
-		for col_index in range(0, len(img1)):
-			new_image[col_index*2] = img1[col_index]
-			new_image[col_index*2 + 1] = img2[col_index]
+		
+		if not opts.swap_columns:
+			new_image = np.empty([img1.shape[0]*2, img1.shape[1], 3])
+			for row_index in range(0, len(img1)):
+				new_image[row_index*2] = img1[row_index]
+				new_image[row_index*2 + 1] = img2[row_index]
+		elif not opts.swap_rows:
+			new_image = np.empty([img1.shape[0], img1.shape[1]*2, 3])
+			for col_index in range(0, img1.shape[1]):
+				new_image[:, col_index*2] = img1[:, col_index]
+				new_image[:, col_index*2 + 1] = img2[:, col_index]
+		else:
+			print("\nCannot use both row and column modes\n")
+			parser.print_help()
+			exit()
+
 		misc.imsave(outfile, new_image)
